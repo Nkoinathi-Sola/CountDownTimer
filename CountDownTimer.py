9422c32654aa072c9d6da4399c5dc28614ffd41b
SNDev <34571022+Nkoinathi-Sola@users.noreply.github.com>
2020-01-23 13:11:44 +0200

    Update CountDownTimer.py

--- CountDownTimer.py
+++ CountDownTimer.py
@@ -1,8 +1,9 @@
-import time
-
-seconds = int(input("Enter secnds"))
-
+import time 
+# importing time exposes the functions e.g sleep()
+# prompt user for input
+seconds = int(input("Enter seconds")) 
+# creat a loop for count down
 for i in range(seconds):
     print(str(seconds - i) + " seconds remaining")
-    time.sleep(1)
+    time.sleep(1) # executes puthon sleep function between each loop
 print("Time Up!")
